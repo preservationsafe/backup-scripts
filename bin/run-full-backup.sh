98afc3408fe1f92f3aff049cba76b99ee72f2926
Geoffrey L. Brimhall <glbrimhall@email.arizona.edu>
2018-11-13 09:17:55 -0700

    Fixed aaws -> aws typo

--- bin/run-full-backup.sh
+++ bin/run-full-backup.sh
@@ -14,8 +14,8 @@ date > "$RUNFILE"
 
 /opt/amanda/bin/amanda-backup-sequential.sh data-continuity
 
-/opt/amanda/bin/aaws-upload.sh "data-continuity preservation-continuity/sequoia" offsite2018.library.arizona.edu wasabi > /var/log/continuity/sync-wasabi/aws-upload.out 2>&1
+/opt/amanda/bin/aws-upload.sh "data-continuity preservation-continuity/sequoia" offsite2018.library.arizona.edu wasabi > /var/log/continuity/sync-wasabi/aws-upload.out 2>&1
 
-/opt/amanda/bin/aaws-upload-only-new.pl "preservation-continuity/ftdca preservation-continuity/preservation-prd-archive preservation-continuity/preserve preservation-continuity/textmining" > /var/log/continuity/sync-wasabi/aws-upload-only-new.out 2>&1
+/opt/amanda/bin/aws-upload-only-new.pl "preservation-continuity/ftdca preservation-continuity/preservation-prd-archive preservation-continuity/preserve preservation-continuity/textmining" > /var/log/continuity/sync-wasabi/aws-upload-only-new.out 2>&1
 
 rm -f "$RUNFILE"
